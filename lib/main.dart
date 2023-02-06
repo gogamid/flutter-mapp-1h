20c5ed6e7207e9261e80ab8acb9c7e19ae6eb8b2
gogamid <gamidliimron@gmail.com>
2023-02-06 20:38:48 +0100

    feat: use material and scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main(){
   runApp(const MyApp());
@@ -10,7 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      home: Scaffold(),
+    );
   }
 }
 
