10072647343a5112c4ecb09e813d3038787cf078
gogamid <gamidliimron@gmail.com>
2023-02-06 21:32:42 +0100

    feat: add ElevatedButton to the center using Center Widget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child:
+          ElevatedButton(onPressed: () {}, child: const Text('Learn Flutter')),
+    );
   }
 }
