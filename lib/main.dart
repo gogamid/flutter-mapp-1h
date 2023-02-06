99a456510acaf280e55a96c8df87069202a8633e
gogamid <gamidliimron@gmail.com>
2023-02-06 20:42:03 +0100

    feat: create RootPage() using stateful widget and return Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      home: RootPage(),
     );
   }
 }
+
+class RootPage extends StatefulWidget {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
 
