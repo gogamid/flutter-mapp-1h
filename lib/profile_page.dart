095612d95d141fcff515b1b911a72f5c3dd1c34b
gogamid <gamidliimron@gmail.com>
2023-02-07 00:15:57 +0100

    feat: create list of profiles using ListView.builder and ListTile

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
+const int itemCount = 20;
+
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+        itemCount: itemCount,
+        itemBuilder: (context, index) {
+          return  ListTile(
+            leading: const Icon(Icons.person),
+            title: Text('Profile ${(index + 1)}'),
+            subtitle: const Text('Profile Page'),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              debugPrint('Profile ${(index + 1)} is clicked');
+            },
+          );
+        });
   }
 }
